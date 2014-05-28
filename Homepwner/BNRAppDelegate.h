c40565f6f84dc4a1d566cc74381944f65b4dcaf0
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-05-27 20:57:47 -0600

    Initial Commit

---
+++ Homepwner/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Homepwner
+//
+//  Created by Sandquist, Cassandra G on 5/27/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
