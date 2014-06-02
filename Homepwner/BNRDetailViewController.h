e26585609ca51338e12e67d745c393e70ae97eb4
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-06-01 22:13:23 -0600

    add detail view controller

---
+++ Homepwner/BNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by Sandquist, Cassandra G on 6/1/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRDetailViewController : UIViewController
+
+@end
