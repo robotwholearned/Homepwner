7997f0434680739f5aba2e1e89e252892bd4e789
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-06-02 02:02:50 -0600

    detail view conforms to navigation delegate and image picker delegate

--- Homepwner/BNRDetailViewController.h
+++ Homepwner/BNRDetailViewController.h
@@ -10,7 +10,7 @@
 
 @class BNRItem;
 
-@interface BNRDetailViewController : UIViewController
+@interface BNRDetailViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
 @property (nonatomic) BNRItem* item;
 
