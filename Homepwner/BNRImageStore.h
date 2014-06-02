b95ed449388456769515f90c7ec5d1453a2fb512
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-06-02 02:15:45 -0600

    create Image Store

---
+++ Homepwner/BNRImageStore.h
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.h
+//  Homepwner
+//
+//  Created by Sandquist, Cassandra G on 6/2/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRImageStore : NSObject
+
+@end
---
+++ Homepwner/BNRImageStore.m
@@ -0,0 +1,13 @@
+//
+//  BNRImageStore.m
+//  Homepwner
+//
+//  Created by Sandquist, Cassandra G on 6/2/14.
+//  Copyright (c) 2014 robotwholearned. All rights reserved.
+//
+
+#import "BNRImageStore.h"
+
+@implementation BNRImageStore
+
+@end
