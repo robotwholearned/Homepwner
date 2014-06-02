03a4dd672bda954cc6aff690480e07bb5c943e49
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-06-02 02:38:18 -0600

    use dictionary shorthand syntax

--- Homepwner/BNRImageStore.m
+++ Homepwner/BNRImageStore.m
@@ -38,12 +38,11 @@
 
 - (void)setImage:(UIImage*)image forKey:(NSString*)key
 {
-    [self.dictionary setObject:image
-                        forKey:key];
+    self.dictionary[key] = image;
 }
 - (UIImage*)imageForKey:(NSString*)key
 {
-    return [self.dictionary objectForKey:key];
+    return self.dictionary[key];
 }
 - (void)deleteImageForKey:(NSString*)key
 {
