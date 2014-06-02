74169220efb08348df3dd5175d71608f423b6743
Sandquist, Cassandra G <Cassandra.Sandquist@pearson.com>
2014-06-02 01:14:29 -0600

    give detail view a title

--- Homepwner/BNRDetailViewController.m
+++ Homepwner/BNRDetailViewController.m
@@ -46,4 +46,9 @@
     item.serialNumber = self.serialNumberField.text;
     item.valueInDollars = [self.valueField.text intValue];
 }
+- (void)setItem:(BNRItem*)item
+{
+    _item = item;
+    self.navigationItem.title = item.itemName;
+}
 @end
